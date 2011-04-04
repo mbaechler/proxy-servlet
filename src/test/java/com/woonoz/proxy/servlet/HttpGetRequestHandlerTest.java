69225dab09d513d6791f9ad18fe07c6f8acc1ad1
maerzbow <maerzbow@gmail.com>
2011-04-04 23:31:28 +0200

    tried to write a testcase w/ mocking HttpServlet* thingies

---
+++ src/test/java/com/woonoz/proxy/servlet/HttpGetRequestHandlerTest.java
@@ -0,0 +1,34 @@
+package com.woonoz.proxy.servlet;
+
+import java.io.IOException;
+import java.net.URL;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.http.client.HttpClient;
+import org.easymock.EasyMock;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class HttpGetRequestHandlerTest {
+
+	@Test @Ignore
+	public void testGet() throws IOException {
+		URL targetServer = new URL("http://www.google.com/");
+		HttpServletRequest request = EasyMock
+				.createMock(HttpServletRequest.class);
+		HttpServletResponse response = (HttpServletResponse)EasyMock.createControl().createMock(HttpServletResponse.class); 
+		HttpClient client = EasyMock.createMock(HttpClient.class);
+		
+		ServletOutputStream servletOutputStream = EasyMock.createMock(ServletOutputStream.class);
+		EasyMock.expect(response.getOutputStream()).andReturn(servletOutputStream);
+
+		HttpGetRequestHandler httpGetRequestHandler = new HttpGetRequestHandler(
+				request, response, targetServer, client);
+		
+		httpGetRequestHandler.execute();
+	}
+
+}
