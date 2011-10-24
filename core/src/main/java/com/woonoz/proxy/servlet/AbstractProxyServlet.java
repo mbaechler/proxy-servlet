e9700d8bae5ad17420f841ca9e8ad85ee051226e
Matthieu Baechler <matthieu.baechler@linagora.com>
2011-10-24 18:34:36 +0200

    - split ProxyServlet in order to create GuiceProxyServlet

---
+++ core/src/main/java/com/woonoz/proxy/servlet/AbstractProxyServlet.java
@@ -0,0 +1,130 @@
+package com.woonoz.proxy.servlet;
+
+import java.io.IOException;
+import java.net.URL;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.http.HttpException;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpRequestInterceptor;
+import org.apache.http.client.protocol.RequestAddCookies;
+import org.apache.http.client.protocol.ResponseProcessCookies;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.protocol.HttpContext;
+
+import com.woonoz.proxy.servlet.base.HttpRequestHandler;
+import com.woonoz.proxy.servlet.config.ProxyServletConfig;
+import com.woonoz.proxy.servlet.http.verb.HttpDeleteRequestHandler;
+import com.woonoz.proxy.servlet.http.verb.HttpGetRequestHandler;
+import com.woonoz.proxy.servlet.http.verb.HttpHeadRequestHandler;
+import com.woonoz.proxy.servlet.http.verb.HttpOptionsRequestHandler;
+import com.woonoz.proxy.servlet.http.verb.HttpPostRequestHandler;
+import com.woonoz.proxy.servlet.http.verb.HttpPutRequestHandler;
+import com.woonoz.proxy.servlet.http.verb.HttpTraceRequestHandler;
+
+public abstract class AbstractProxyServlet extends HttpServlet {
+
+	private static final int HTTP_DEFAULT_PORT = 80;
+	private URL targetServer;
+	private DefaultHttpClient client;
+
+	public AbstractProxyServlet() {
+		super();
+	}
+
+	public void init(ProxyServletConfig config) {
+		targetServer = config.getTargetUrl();
+	    if (targetServer != null) {
+	        SchemeRegistry schemeRegistry = new SchemeRegistry();
+	        schemeRegistry.register(new Scheme(targetServer.getProtocol(), getPortOrDefault(targetServer.getPort()), PlainSocketFactory.getSocketFactory()));
+	        BasicHttpParams httpParams = new BasicHttpParams();
+	        HttpConnectionParams.setConnectionTimeout( httpParams, config.getConnectionTimeout() );
+	        HttpConnectionParams.setSoTimeout( httpParams, config.getSocketTimeout() );
+	        ThreadSafeClientConnManager cm = new ThreadSafeClientConnManager(schemeRegistry);
+	        cm.setDefaultMaxPerRoute( config.getMaxConnections() );
+	        cm.setMaxTotal( config.getMaxConnections() );
+	        client = new DefaultHttpClient(cm, httpParams);
+	        client.removeResponseInterceptorByClass(ResponseProcessCookies.class);
+	        client.removeRequestInterceptorByClass(RequestAddCookies.class);
+	        
+	        final String remoteUserHeader = config.getRemoteUserHeader(); 
+	        if (null != remoteUserHeader) {
+	            client.addRequestInterceptor(new HttpRequestInterceptor() {
+	                
+	                @Override
+	                public void process(HttpRequest request, HttpContext context) throws HttpException, IOException {
+	                    request.removeHeaders(remoteUserHeader);
+	                    HttpRequestHandler handler;
+	                    if (context != null && (handler = (HttpRequestHandler) context.getAttribute(HttpRequestHandler.class.getName())) != null) {
+	                        String remoteUser = handler.getRequest().getRemoteUser();
+	                        if (remoteUser != null) {
+	                            request.addHeader(remoteUserHeader, remoteUser);
+	                        }
+	                    }
+	                }
+	            });
+	        }
+	    }
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+			IOException {
+				new HttpGetRequestHandler(request, response, targetServer, client).execute();
+			}
+
+	@Override
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+				new HttpDeleteRequestHandler(request, response, targetServer, client).execute();
+			}
+
+	@Override
+	protected void doHead(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+			IOException {
+				new HttpHeadRequestHandler(request, response, targetServer, client).execute();
+			}
+
+	@Override
+	protected void doOptions(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+				new HttpOptionsRequestHandler(request, response, targetServer, client).execute();
+			}
+
+	@Override
+	protected void doTrace(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+				new HttpTraceRequestHandler(request, response, targetServer, client).execute();
+			}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+			IOException {
+				new HttpPostRequestHandler(request, response, targetServer, client).execute();
+			}
+
+	@Override
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+			IOException {
+				new HttpPutRequestHandler(request, response, targetServer, client).execute();
+			}
+
+	private int getPortOrDefault(int port) {
+		if (port == -1) {
+			return HTTP_DEFAULT_PORT;
+		} else {
+			return port;
+		}
+	}
+
+}
